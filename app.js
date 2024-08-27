4b486c49c49ed8d240a09e92ec56c5a7291276cf
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-08-27 17:57:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { contentType, status } = require('express/lib/response');
+const app = express();
+const fs = require('fs');
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  })
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('app running on port: ' + port);
+});
