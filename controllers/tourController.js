e2b0bf200419a2919f7d88d27b849086942d457c
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-03-18 13:19:29 +0500

    create api features class in utils for querying tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,48 +11,16 @@ exports.aliasTopTours = (req, res, next) => {
 // Get All Tours
 exports.getTours = async (req, res) => {
   try {
-    // 1.1) Filtering
-    const queryObj = { ...req.query };
-    const excludeFilters = ['limit', 'sort', 'page', 'fields'];
-    excludeFilters.forEach((el) => delete queryObj[el]);
+    // Execute Query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // 1.2) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
+    const tours = await features.query;
 
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-
-      query = query.sort(sortBy);
-    } else {
-      query.sort('-ratingsAverage');
-    }
-
-    // 3) Fields
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-      console.log(fields);
-    } else {
-      query.select('-__v');
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const docs = await Tour.countDocuments();
-      if (skip >= docs) {
-        throw new Error('This page does not exist');
-      }
-    }
-    const tours = await query;
+    // Send Query
     res.status(200).json({
       status: 'success',
       requestedAt: req.time,
