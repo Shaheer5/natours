8c2d76a35cbc21d1ac54e5aa0983f452329f3f9c
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-03-27 16:27:37 +0500

    add new route: monthly plan for business stats

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -136,3 +136,48 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTours: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $sort: { numTours: -1 },
+      },
+      {
+        $project: { _id: 0 },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: plan,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
