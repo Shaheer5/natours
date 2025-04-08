ee3027fcfd508d77fadc69eb38f6b2f27aab47b0
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-08 13:26:05 +0500

    add middleware for unhandled routes in the app

--- app.js
+++ app.js
@@ -23,4 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: '404 not found',
+    message: `The request ${req.originalUrl} is not found on server`,
+  });
+  next();
+});
 module.exports = app;
