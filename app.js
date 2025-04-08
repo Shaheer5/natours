e971b15a74daca3725a7f77612b7a57dd48055ac
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-08 17:16:50 +0500

    update handling unhandled routes and add globalErrorHandlers

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandlers = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -23,11 +26,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handling unhandled routes (operational errors)
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: '404 not found',
-    message: `The request ${req.originalUrl} is not found on server`,
-  });
-  next();
+  next(new AppError(`The request ${req.originalUrl} is not found on server`));
 });
+app.use(globalErrorHandlers);
+
 module.exports = app;
