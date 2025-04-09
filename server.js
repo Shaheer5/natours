7a3856f570de8340479b653daa17bd6494ea66b8
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-09 18:14:17 +0500

    shift process.exit inside the server.close callback

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
-  server.close();
-  process.exit(1);
+  server.close(() => {
+    process.exit(1);
+  });
 });
