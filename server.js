d5d7eb972961289e289f7ed77d0b2b70bd65d272
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-09 18:12:21 +0500

    add unhandledRecjection process to close and exit the server

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ mongoose
   );
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log('app running on port: ' + port);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close();
+  process.exit(1);
 });
