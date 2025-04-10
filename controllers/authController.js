a1fc437d302128c04aabed450c099f4139b19f25
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-10 16:30:41 +0500

    install jwt and add to user signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,20 @@
 const catchAsyncError = require('../utils/catchAsyncError');
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
