fee81068f0cfa1936a3794328adb5c01cac842e0
Shaheer Mansoor <shaheermansoor321@gmail.com>
2025-04-10 14:00:56 +0500

    add signup controller for users/signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,12 @@
+const catchAsyncError = require('../utils/catchAsyncError');
 const User = require('./../models/userModel');
+
+exports.signup = catchAsyncError(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+router.post('/signup', authController.signup)
 
 router.param('id', userController.checkId);
-
 router.route('/').get(userController.getUsers).post(userController.createUser);
 router
   .route('/:id')
